e68c2fcd14347401127d10dae8906c7ec2ca12b3
fredmadrona <fredriccamasis.madrona@bicol-u.edu.ph>
2024-05-01 23:07:02 +0800

    added other categories in homepage

--- src/components/HomePageCategories.js
+++ src/components/HomePageCategories.js
@@ -20,14 +20,14 @@ export default function HomePagecategories() {
             Categories
           </h1>{" "}
         </div>
-        <div className="md:col-span-2 col-span-1 "></div>
+        <div className="col-span-1 "></div>
       </div>
       <div className="grid grid-cols-12 lg:p-5 p-1">
         <div className="md:col-span-2 hidden md:flex "></div>
 
-        <div className="md:col-span-8 col-span-12 flex flex-row justify-center md:gap-3 gap-1 p-1">
+        <div className="md:col-span-8 col-span-12 grid grid-cols-3 justify-center md:gap-3 gap-1 p-1">
           <div
-            className=" text-center bg-white p-3 flex flex-col justify-between cursor-pointer"
+            className=" text-center bg-white p-3 col-span-1 flex flex-col justify-between cursor-pointer"
             onClick={() => handleMenuItemClick("Door and Window")}
           >
             <Image
@@ -39,7 +39,7 @@ export default function HomePagecategories() {
             <span className="text-lg text-center text-gray-500"> Door </span>
           </div>
           <div
-            className=" text-center bg-white p-3 flex flex-col justify-between cursor-pointer"
+            className=" text-center bg-white p-3 col-span-1 flex flex-col justify-between cursor-pointer"
             onClick={() => handleMenuItemClick("Flooring")}
           >
             <Image
@@ -54,7 +54,82 @@ export default function HomePagecategories() {
             </span>
           </div>
           <div
-            className="border text-center bg-white p-3 flex flex-col justify-between cursor-pointer"
+            className="border text-center bg-white p-3 col-span-1 flex flex-col justify-between cursor-pointer"
+            onClick={() => handleMenuItemClick("LED Screen")}
+          >
+            <Image
+              src="/LEDScreen(2).png"
+              width={125}
+              height={150}
+              alt="LED ScreenImage"
+            />
+            <span className="text-lg text-center text-gray-500">
+              {" "}
+              LED Screen{" "}
+            </span>
+          </div>
+          <div
+            className="border text-center bg-white p-3 col-span-1 flex flex-col justify-between cursor-pointer"
+            onClick={() => handleMenuItemClick("Outdoor Lighting")}
+          >
+            <Image
+              src="/OutdoorLighting(1).png"
+              width={125}
+              height={150}
+              alt="Outdoor Lighting Image"
+            />
+            <span className="text-lg text-center text-gray-500">
+              {" "}
+             Outdoor Lighting{" "}
+            </span>
+          </div>
+          <div
+            className="border text-center bg-white p-3 col-span-1 flex flex-col justify-between cursor-pointer"
+            onClick={() => handleMenuItemClick("Pipeline")}
+          >
+            <Image
+              src="/Pipeline(2).png"
+              width={125}
+              height={150}
+              alt="Pipeline Image"
+            />
+            <span className="text-lg text-center text-gray-500">
+              {" "}
+              Pipeline{" "}
+            </span>
+          </div>
+          <div
+            className="border text-center bg-white p-3 col-span-1 flex flex-col justify-between cursor-pointer"
+            onClick={() => handleMenuItemClick("Safety Products")}
+          >
+            <Image
+              src="/SafetyProduct(1).png"
+              width={125}
+              height={150}
+              alt="Safety Products Image"
+            />
+            <span className="text-lg text-center text-gray-500">
+              {" "}
+              Safety Products{" "}
+            </span>
+          </div>
+          <div
+            className="border text-center bg-white p-3 col-span-1 flex flex-col justify-between cursor-pointer"
+            onClick={() => handleMenuItemClick("Smart Devices")}
+          >
+            <Image
+              src="/SmartDevice(1).png"
+              width={125}
+              height={150}
+              alt="Smart Products Image"
+            />
+            <span className="text-lg text-center text-gray-500">
+              {" "}
+              Smart Products{" "}
+            </span>
+          </div> 
+          <div
+            className="border text-center bg-white p-3 col-span-1 flex flex-col justify-between cursor-pointer"
             onClick={() => handleMenuItemClick("Tile Products")}
           >
             <Image
@@ -67,20 +142,20 @@ export default function HomePagecategories() {
               {" "}
               Tile Products{" "}
             </span>
-          </div>
-          <div
-            className="border text-center bg-white p-3 flex flex-col justify-between cursor-pointer"
-            onClick={() => handleMenuItemClick("Smart Devices")}
+          </div>      
+           <div
+            className="border text-center bg-white p-3 col-span-1 flex flex-col justify-between cursor-pointer"
+            onClick={() => handleMenuItemClick("Others")}
           >
             <Image
-              src="/SmartDevice(1).png"
+              src="/Hardware02.jpg"
               width={125}
               height={150}
-              alt="Smart Products Image"
+              alt="Door Stopper Image"
             />
             <span className="text-lg text-center text-gray-500">
               {" "}
-              Smart Products{" "}
+             Others{" "}
             </span>
           </div>
         </div>
