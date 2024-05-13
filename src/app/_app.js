24dcbb9e2172c0e8ca497da959caae4d3807934f
Mark <renzomndz09@gmail.com>
2024-05-13 21:25:51 +0800

    installed SWR

--- src/app/_app.js
+++ src/app/_app.js
@@ -2,6 +2,7 @@
 import { CartProvider } from "@/components/CartContext"; // Adjust the path as needed
 import "../styles/globals.css";
 
+
 function MyApp({ Component, pageProps }) {
   return (
     <CartProvider>
