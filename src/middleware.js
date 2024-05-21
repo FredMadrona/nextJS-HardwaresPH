0aee53a33549232f6ccd2cfdc7d9d200c8ffe022
fredmadrona <fredriccamasis.madrona@bicol-u.edu.ph>
2024-05-21 22:32:58 +0800

    added middleware

---
+++ src/middleware.js
@@ -0,0 +1,25 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    const token = req.nextauth.token;
+
+    if (!token && req.nextUrl.pathname !== "/") {
+      return NextResponse.redirect(new URL("/auth/signin", req.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
+
+export const config = {
+matcher: [
+  "/cart",
+  "/checkout",
+  "/products"
+]
+};
